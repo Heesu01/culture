54fa9390e342821c72e6e49da605a1ba3370ea57
Heesu01 <rulru0413@naver.com>
2025-06-27 17:55:36 +0900

    feat: 버튼 공통 컴포넌트 구현

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -1,5 +1,43 @@
-const Button = () => {
-  return <div></div>;
+import type { ButtonProps } from "@/shared/types/button";
+
+const Button = ({
+  children,
+  onClick,
+  type = "button",
+  variant = "primary",
+  disabled = false,
+  className = "",
+}: ButtonProps) => {
+  const baseStyle = `
+    w-full max-w-[500px]
+    h-[60px]
+    rounded-[16px]
+    text-subtitle
+    text-center
+    transition-colors
+  `;
+
+  const variants = {
+    primary: "bg-primary text-white hover:bg-primary/80",
+    secondary: "bg-deactivate text-subtext hover:bg-deactivate/80",
+  };
+
+  const disabledStyle = `
+    bg-deactivate text-deactivate-text cursor-not-allowed opacity-60
+  `;
+
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`${baseStyle} ${
+        disabled ? disabledStyle : variants[variant]
+      } ${className}`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
