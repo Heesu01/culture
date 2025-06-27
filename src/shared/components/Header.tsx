163c64db3b58f00c0ce702a024bed7ac8ce86c81
Heesu01 <rulru0413@naver.com>
2025-06-27 23:14:42 +0900

    feat: 공통 헤더 컴포넌트 구현

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,5 +1,31 @@
-const Header = () => {
-  return <div></div>;
+import { useNavigate } from "react-router-dom";
+import backImg from "@/assets/back.png";
+
+interface CommonHeaderProps {
+  title: string;
+  showBack?: boolean;
+}
+
+const Header = ({ title, showBack = true }: CommonHeaderProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="relative flex items-center h-[44px] mt-[44px]">
+      {showBack && (
+        <button className="absolute left-[32px]" onClick={() => navigate(-1)}>
+          <img
+            src={backImg}
+            alt="뒤로가기"
+            className="w-6 h-6 object-contain"
+          />
+        </button>
+      )}
+
+      <span className="text-subtitle1 absolute left-1/2 -translate-x-1/2">
+        {title}
+      </span>
+    </div>
+  );
 };
 
 export default Header;
