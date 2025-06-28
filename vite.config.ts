4bffe7c393cca0b24f8f0160039704dfb25d56d0
Heesu01 <rulru0413@naver.com>
2025-06-29 01:09:08 +0900

    hotfix: 배포 빌드 에러 해결

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,30 @@ import fs from "fs";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "cert/localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "cert/localhost.pem")),
-    },
-    port: 3000,
-  },
+    server: isDev
+      ? {
+          https: {
+            key: fs.readFileSync(
+              path.resolve(__dirname, "cert/localhost-key.pem")
+            ),
+            cert: fs.readFileSync(
+              path.resolve(__dirname, "cert/localhost.pem")
+            ),
+          },
+          port: 3000,
+        }
+      : {
+          port: 3000,
+        },
+  };
 });
