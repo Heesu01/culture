063e4821c974c0e11f58ebc3bc59b09989981c74
Heesu01 <rulru0413@naver.com>
2025-07-03 19:05:36 +0900

    fix: 어세스토큰 관리 수정

--- src/features/mypage/pages/MyPage.tsx
+++ src/features/mypage/pages/MyPage.tsx
@@ -16,6 +16,7 @@ const MyPage = () => {
     try {
       const res = await logout();
       console.log("로그아웃 성공:", res.message);
+      localStorage.removeItem("accessToken");
       alert(res.message || "로그아웃 되었습니다.");
       navigate("/login");
     } catch (error) {
