8bef1386318213aa1aa4ef79ec8f81dcf8fe2850
Heesu01 <rulru0413@naver.com>
2025-07-09 21:32:29 +0900

    feat: JWT 만료 시간 검사 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import NavBar from "./shared/components/NavBar";
+import { useEffect } from "react";
 
 const App = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const visiblePaths = ["/market", "/mypage", "/stamp"];
   const showNav =
     visiblePaths.includes(location.pathname) ||
     location.pathname.startsWith("/market/");
+
+  useEffect(() => {
+    const isTokenExpired = (token: string | null) => {
+      if (!token) return true;
+
+      try {
+        const payload = JSON.parse(atob(token.split(".")[1]));
+        const expirationTime = payload.exp * 1000;
+        return Date.now() > expirationTime;
+      } catch {
+        return true;
+      }
+    };
+
+    const token = localStorage.getItem("accessToken");
+
+    if (isTokenExpired(token)) {
+      localStorage.removeItem("accessToken");
+      navigate("/login");
+    }
+  }, [location.pathname, navigate]);
 
   return (
     <div className="flex flex-col min-h-screen pt-[56px]">
